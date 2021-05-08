b9ce50a25edb097fe205b7ea010aecb79aba815e
Martes Carry <carrymartes@gmail.com>
2021-05-08 23:13:16 +0500

    app service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,30 +1,24 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
+//import this
 use Illuminate\Support\Facades\Schema;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-        Schema::defaultStringLength(191);
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider{
+  /**
+  * Register any application services.
+  *
+  * @return void
+  */
+  public function register(){
+     //add this
+     Schema::defaultStringLength(191);
+  }
+  /**
+  * Bootstrap any application services.
+  *
+  * @return void
+  */
+   public function boot(){
+      //
+   }
 }
