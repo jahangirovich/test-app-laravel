55c11a51a63bd2d54f50a38c03726655c3d03502
Martes Carry <carrymartes@gmail.com>
2021-05-12 16:51:41 +0500

    asdasdad

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use App\Http\Controllers\API\AuthController;
 |
 */
 
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
